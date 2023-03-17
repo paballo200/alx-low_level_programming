d301eb993990b7ce94dc36f529c37883ce3e716f
paballo <molokommep81@gmail.com>
2023-03-17 08:08:31 -0700

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - this prints "Programming is like building a multilingual puzzle
- * return: this returns 0
+ * main - A program that prints hello world
+ * return: (0)
  */
-
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
