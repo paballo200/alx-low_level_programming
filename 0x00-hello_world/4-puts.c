98dc217e2d3ff009e78ff63d51085742a5905292
paballo <molokommep81@gmail.com>
2023-03-17 07:37:20 -0700

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,3 +4,9 @@
  * main - this prints "Programming is like building a multilingual puzzle
  * return: this returns 0
  */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
