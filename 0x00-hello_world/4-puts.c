e75bfe466abb471ab5cca89a9ff775f6a7db3afb
paballo <molokommep81@gmail.com>
2023-03-18 03:42:34 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - A program that prints hello world
- * return: (0)
+ * main - hello world
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
