7661b77f795bdbad6405b6ca8d5d4f2479807325
paballo <molokommep81@gmail.com>
2023-03-20 10:43:14 -0700

    size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of char:%ld byte(s)\n", sizeof(char));
-	printf("size of int:%ld byte(s)\n", sizeof(int));
-	printf("size of long int:%ld byte(s)\n", sizeof(long int));
-	printf("size of long long int:%ld byte(s)\n", sizeof(long long int));
-	printf("size of float:%ld byte(s)\n", sizeof(float));
+	printf("size of char:%c byte(s)\n", sizeof(char));
+	printf("size of int:%d byte(s)\n", sizeof(int));
+	printf("size of long int:%lu byte(s)\n", sizeof(long int));
+	printf("size of long long int:%llu byte(s)\n", sizeof(long long int));
+	printf("size of float:%f byte(s)\n", sizeof(float));
 	return (0);
 }
