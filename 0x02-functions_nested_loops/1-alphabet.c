4e6973dbab3865e4a6a91acd8e56fb6fa10f15ab
paballo <molokommep81@gmail.com>
2023-03-22 07:59:24 -0700

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char p;
+
+	for (p = 'a' ; p <= 'z'  ; p++)
+		_putchar(p);
+	_putchar('\n');
+	return (0);
+}
