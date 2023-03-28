ede49126829c728061dfe199b936e0a71de0776f
paballo <molokommep81@gmail.com>
2023-03-28 08:01:53 -0700

    98 bettry st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates it to 98
+ * @n: pointer to int
+ *
+ * Return: 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
